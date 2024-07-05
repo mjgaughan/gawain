03efe17fd0ab5f19bbbb5e7312048bd1cc695938
mjgaughan <mgaughan@proton.me>
2024-07-05 11:15:25 -0400

    adding list flag

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,32 @@ use backends::git;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let target = Target::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    println!("Looking for project {}", target.vcs_link);
-
-    if target.vcs_type != "git"{
-        println!("Sorry! Only git right now.");
-        process::exit(1);
+    let mut is_list = false;
+    if args[2] == "-l"{
+        println!("list!");
+        is_list = true;
+    } else {
+        println!("not list!");
     }
 
-    let git_results = git::GitRepository::new(target).unwrap_or_else(|err| {
-        println!("Problem with git data: {}", err);
-        process::exit(1);
-    });
-    println!("{:?}",git_results);
+    if !is_list {
+        let target = Target::new(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        });
+    
+        println!("Looking for project {}", target.vcs_link);
+    
+        if target.vcs_type != "git"{
+            println!("Sorry! Only git right now.");
+            process::exit(1);
+        }
+    
+        let git_results = git::GitRepository::new(target).unwrap_or_else(|err| {
+            println!("Problem with git data: {}", err);
+            process::exit(1);
+        });
+        println!("{:?}",git_results);
+    }
+
 }
